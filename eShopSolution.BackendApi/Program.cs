aaa8e33c6d3b33738eef9fc4b7f3881650dee9a6
hieubuitrung <buitrunghieu15112001@gmail.com>
2023-02-25 22:38:52 +0700

    update

--- eShopSolution.BackendApi/Program.cs
+++ eShopSolution.BackendApi/Program.cs
@@ -1,7 +1,10 @@
 using eShopSolution.Application.Catalog.Products;
 using eShopSolution.Application.Common;
+using eShopSolution.Application.System.Users;
 using eShopSolution.Data.EF;
+using eShopSolution.Data.Entities;
 using eShopSolution.Utilities.Constants;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 
@@ -14,11 +17,18 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<EShopDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString(SystemConstants.MainConnectionString)));
 
+builder.Services.AddIdentity<AppUser, AppRole>()
+    .AddEntityFrameworkStores<EShopDbContext>()
+    .AddDefaultTokenProviders();
 // Declare DI
 // If requesting an object IPublicProductService it will create instance PublicProductService
 builder.Services.AddTransient<IPublicProductService, PublicProductService>();
 builder.Services.AddTransient<IManageProductService, ManageProductService>();
 builder.Services.AddTransient<IStorageService, FileStorageService>();
+builder.Services.AddTransient<UserManager<AppUser>, UserManager<AppUser>>();
+builder.Services.AddTransient<SignInManager<AppUser>, SignInManager<AppUser>>();
+builder.Services.AddTransient<RoleManager<AppRole>, RoleManager<AppRole>>();
+builder.Services.AddTransient<IUserService, UserService>();
 
 
 // Add services to the container.
